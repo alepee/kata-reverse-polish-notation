88cf887c0e2b3f9ebd6f1a32dec4cde30edcdf53
Antoine Lépée <alepee@gmail.com>
2018-02-15 16:49:24 +0100

    refacto: PolishNotationCalculator as a class

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,8 +1,12 @@
 require 'rspec'
 
-module PolishNotationCalculator
-  def self.evaluate(formula)
-    formula.split.inject([]) { |acc, item|
+class PolishNotationCalculator
+  def initialize(formula)
+    @formula = formula
+  end
+
+  def evaluate
+    @formula.split.inject([]) { |acc, item|
       if item.match(/\d+/)
         acc.push(item.to_i)
       else
@@ -24,63 +28,63 @@ end
 describe 'Reverse Polish Notation' do
   it 'will sum two values' do
     entry_value = '3 5 +'
-    sut = PolishNotationCalculator.evaluate(entry_value)
+    sut = PolishNotationCalculator.new(entry_value).evaluate
 
     expect(sut).to eq 8
   end
 
   it 'will sum three values' do
     entry_value = '4 3 2 + +'
-    sut = PolishNotationCalculator.evaluate(entry_value)
+    sut = PolishNotationCalculator.new(entry_value).evaluate
 
     expect(sut).to eq 9
   end
 
   it 'will sum two values with one negative value' do
     entry_value = '-50 5 +'
-    sut = PolishNotationCalculator.evaluate(entry_value)
+    sut = PolishNotationCalculator.new(entry_value).evaluate
 
     expect(sut).to eq -45
   end
 
   it 'will subtract two values' do
     entry_value = '5 2 -'
-    sut = PolishNotationCalculator.evaluate(entry_value)
+    sut = PolishNotationCalculator.new(entry_value).evaluate
 
     expect(sut).to eq 3
   end
 
   it 'will sum and subtract three values' do
     entry_value = '5 2 - 7 +'
-    sut = PolishNotationCalculator.evaluate(entry_value)
+    sut = PolishNotationCalculator.new(entry_value).evaluate
 
     expect(sut).to eq 10
   end
 
   it 'will sum and subtract three values' do
     entry_value = '5 2 - 0 +'
-    sut = PolishNotationCalculator.evaluate(entry_value)
+    sut = PolishNotationCalculator.new(entry_value).evaluate
 
     expect(sut).to eq 3
   end
 
   it 'will divide two values' do
     entry_value = '6 2 /'
-    sut = PolishNotationCalculator.evaluate(entry_value)
+    sut = PolishNotationCalculator.new(entry_value).evaluate
 
     expect(sut).to eq 3
   end
 
   it 'will raise ZeroDivisionError exception' do
     entry_value = '6 0 /'
-    sut = -> { PolishNotationCalculator.evaluate(entry_value) }
+    sut = -> { PolishNotationCalculator.new(entry_value).evaluate }
 
     expect(sut).to raise_error(ZeroDivisionError)
   end
 
-  it 'will ' do
+  it 'will evaluate a formula with multiple operators' do
     entry_value = '3 4 2 1 + * + 2 /'
-    sut = PolishNotationCalculator.evaluate(entry_value)
+    sut = PolishNotationCalculator.new(entry_value).evaluate
 
     expect(sut).to eq 7.5
   end
